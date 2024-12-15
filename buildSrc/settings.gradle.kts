fba6aa19c9f5f559408ac8176272c57148b4146f
Alejandro Rioseco <844938@unizar.es>
2024-12-15 19:48:48 +0100

    g

--- buildSrc/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            // Specifies the location of the version catalog file.
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
--- settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Set the name of the root project
-rootProject.name = "urlshortener"
-
-// Include the specified subprojects in the build
-include("core", "delivery", "repositories", "app")
